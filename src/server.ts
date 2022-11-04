67d14d1d21b79fa80f48665513c9900115f01a7e
AlessandraMayumi <alessandra_mms@hotmail.com>
2022-11-04 16:13:23 -0300

    prisma diagram ERD and fastify CORS

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,27 @@
-import Fastify from "fastify";
+import Fastify from "fastify"
+import cors from "@fastify/cors"
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
 
 async function bootstrap() {
     const fastify = Fastify({
         logger: true,
     })
 
-    fastify.get('/pools/count', () => {
-        return { count: 153451 }
+    await fastify.register(cors, {
+        origin: true,
     })
 
-    await fastify.listen({ port: 3333 })
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count()
+
+        return { count }
+    })
+
+    await fastify.listen({ host: '0.0.0.0', port: 3333 })
 }
 
 bootstrap()
