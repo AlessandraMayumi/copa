9cfd06c43b6203bde8be840fed9b3538ddcc429f
AlessandraMayumi <alessandra_mms@hotmail.com>
2022-11-05 10:48:55 -0300

    UI: fetch count

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,9 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+interface HomeProps {
+  count: number,
+}
 
-export default function Home() {
+export default function Home(props: HomeProps) {
   return (
-    <h1>Hello World</h1>
+    <h1>Contagem: {props.count}</h1>
   )
+}
+
+export const getServerSideProps = async() => {
+  const response = await fetch('http://0.0.0.0:3333/pools/count')
+  const data = await response.json()
+
+  return {
+    props: {
+      count: data.count,
+    }
+  }
 }
